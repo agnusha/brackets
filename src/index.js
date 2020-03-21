d392e08a3b1274d0271698ade1f47bd808bfb519
Agnia <agnia2000@inbox.ru>
2020-03-21 18:10:49 +0300

    feat: finish solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 === 1) return false;
+    for (let i = 0; i < bracketsConfig.length;) {
+        let curBracket = bracketsConfig[i].join('');
+        if (!str.includes(curBracket))
+            i++;
+        else {
+            str = str.replace(curBracket, '')
+            i = 0;
+        }
+    }
+    return str.length == 0;
 }
